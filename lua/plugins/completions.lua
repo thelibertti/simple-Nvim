4882c9122c29fe06b62aaa3bbd626e61f8f238ac
thelibertti <139710771+thelibertti@users.noreply.github.com.>
2024-05-28 16:11:47 -0600

    [Add] added symbols to the cmp window

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -18,34 +18,69 @@ return {
       local cmp = require("cmp")
       require("luasnip.loaders.from_vscode").lazy_load()
 
+      local lsp_symbols = {
+        Text = "   (Text) ",
+        Method = "  (Method)",
+        Function = " 󰊕  (Function)",
+        Constructor = "   (Constructor)",
+        Field = "   (Field)",
+        Variable = "[] (Variable)",
+        Class = "   (Class)",
+        Interface = "   (Interface)",
+        Module = "   (Module)",
+        Property = "  (Property)",
+        Unit = "   (Unit)",
+        Value = "   (Value)",
+        Enum = "  (Enum)",
+        Keyword = "   (Keyword)",
+        Snippet = "   (Snippet)",
+        Color = "   (Color)",
+        File = "   (File)",
+        Reference = "   (Reference)",
+        Folder = "   (Folder)",
+        EnumMember = "   (EnumMember)",
+        Constant = "   (Constant)",
+        Struct = "   (Struct)",
+        Event = "   (Event)",
+        Operator = "   (Operator)",
+        TypeParameter = " 󰉺  (TypeParameter)",
+      }
+
       cmp.setup({
+
         snippet = {
           expand = function(args)
             require("luasnip").lsp_expand(args.body)
           end,
         },
-        window = {
-          completion = cmp.config.window.bordered(),
-          documentation = cmp.config.window.bordered(),
-        },
-        mapping = cmp.mapping.preset.insert({
-          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-          ["<C-f>"] = cmp.mapping.scroll_docs(4),
-          ["<C-Space>"] = cmp.mapping.complete(),
-          ["<C-e>"] = cmp.mapping.abort(),
-          ["<CR>"] = cmp.mapping.confirm({ select = true }),
 
-        }),
-        sources = cmp.config.sources({
+        sources = {
+          { name = "buffer" },
           { name = "nvim_lsp" },
           { name = "luasnip" },
-        }, {
-          { name = "buffer" },
-        }),
-      })
+        },
+
+      mapping = cmp.mapping.preset.insert({
+      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+      ['<C-f>'] = cmp.mapping.scroll_docs(4),
+      ['<C-Space>'] = cmp.mapping.complete(),
+      ['<C-e>'] = cmp.mapping.abort(),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }),
+      }),
+
+      formatting = {
+        format = function(entry, item)
+            item.kind = lsp_symbols[item.kind]
+            item.menu = ({
+                buffer = "[Buffer]",
+                nvim_lsp = "[LSP]",
+                luasnip = "[Snippet]",
+            })[entry.source.name]
+
+            return item
+        end,
+      },
+    })
     end,
   },
 }
-
-
---ready for alpha 0.3
