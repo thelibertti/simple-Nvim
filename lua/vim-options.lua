9c5bd57b7d41f4fb777ee2d05610b9ed91c5fd60
thelibertti <139710771+thelibertti@users.noreply.github.com.>
2024-06-01 16:17:44 -0600

    [Modify] modify some settings

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -10,11 +10,9 @@ vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
 vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
 vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
 
-
-vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
 vim.wo.number = true
 
---icons for errors and stuff
+--icons for errors and other stuff
 vim.fn.sign_define("DiagnosticSignError",
     {text = " ", texthl = "DiagnosticSignError"})
 vim.fn.sign_define("DiagnosticSignWarn",
